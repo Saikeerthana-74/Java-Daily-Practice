98fd7b2d12d68487f13253b35e18d48764f95a85
Sai Keerthana <b23it074@kitsw.ac.in>
2026-05-31 21:11:50 +0530

    Added Duplicate Characters Java program

---
+++ Strings/DuplicateCharacters.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class DuplicateCharacters {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+
+        System.out.println("Duplicate Characters:");
+
+        for(int i = 0; i < str.length(); i++) {
+            for(int j = i + 1; j < str.length(); j++) {
+                if(str.charAt(i) == str.charAt(j)) {
+                    System.out.println(str.charAt(i));
+                    break;
+                }
+            }
+        }
+    }
+}
