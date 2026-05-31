18339ad8d622dde2b7753fb70b5c30c8ae56185d
Sai Keerthana <b23it074@kitsw.ac.in>
2026-05-31 21:06:27 +0530

    Palindrom string

---
+++ Strings/PalindromeString.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class PalindromeString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String s = sc.nextLine();
+
+        String rev = "";
+
+        for(int i = s.length()-1; i >= 0; i--) {
+            rev = rev + s.charAt(i);
+        }
+
+        if(s.equals(rev))
+            System.out.println("Palindrome String");
+        else
+            System.out.println("Not a Palindrome String");
+    }
+}
