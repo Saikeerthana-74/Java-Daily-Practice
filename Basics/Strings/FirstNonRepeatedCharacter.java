e6107e01fd3086865acc8b37ab88498f6cca3cac
Sai Keerthana <b23it074@kitsw.ac.in>
2026-06-06 21:49:33 +0530

    Added First Non Repeated Character Java program

---
+++ Basics/Strings/FirstNonRepeatedCharacter.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class FirstNonRepeatedCharacter {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+
+        boolean found = false;
+
+        for(int i = 0; i < str.length(); i++) {
+            int count = 0;
+
+            for(int j = 0; j < str.length(); j++) {
+                if(str.charAt(i) == str.charAt(j)) {
+                    count++;
+                }
+            }
+
+            if(count == 1) {
+                System.out.println("First Non-Repeated Character: " + str.charAt(i));
+                found = true;
+                break;
+            }
+        }
+
+        if(!found) {
+            System.out.println("No Non-Repeated Character Found");
+        }
+    }
+}
