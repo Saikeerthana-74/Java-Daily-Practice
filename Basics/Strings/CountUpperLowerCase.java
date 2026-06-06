6d27bb868bc1157a7b90be1eb44d9bdf5bab8c90
Sai Keerthana <b23it074@kitsw.ac.in>
2026-06-06 21:48:39 +0530

    Added Count Upper and Lower Case Java program

---
+++ Basics/Strings/CountUpperLowerCase.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class CountUpperLowerCase {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+
+        int upper = 0, lower = 0;
+
+        for(int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+
+            if(ch >= 'A' && ch <= 'Z')
+                upper++;
+            else if(ch >= 'a' && ch <= 'z')
+                lower++;
+        }
+
+        System.out.println("Uppercase letters: " + upper);
+        System.out.println("Lowercase letters: " + lower);
+    }
+}
