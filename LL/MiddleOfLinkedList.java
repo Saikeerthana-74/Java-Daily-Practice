9d94dae63885899719546f1e470789793a09efef
Sai Keerthana <b23it074@kitsw.ac.in>
2026-06-20 18:47:36 +0530

    Implement Middle of Linked List finder

---
+++ LL/MiddleOfLinkedList.java
@@ -0,0 +1,36 @@
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+public class MiddleOfLinkedList {
+
+    public static void findMiddle(Node head) {
+
+        Node slow = head;
+        Node fast = head;
+
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        System.out.println("Middle Node: " + slow.data);
+    }
+
+    public static void main(String[] args) {
+
+        Node head = new Node(10);
+        head.next = new Node(20);
+        head.next.next = new Node(30);
+        head.next.next.next = new Node(40);
+        head.next.next.next.next = new Node(50);
+
+        findMiddle(head);
+    }
+}
